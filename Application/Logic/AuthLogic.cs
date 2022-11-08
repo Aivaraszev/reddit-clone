3072c95c703b42543c11e1fb5260bb966b9d2319
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-08 18:23:24 +0100

    Add an authentication logic interface and implement it.

---
+++ Application/Logic/AuthLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain.Models;
+
+namespace Application.Logic;
+
+public class AuthLogic : IAuthLogic
+{
+    private readonly IUserDao _userDao;
+
+    public AuthLogic(IUserDao userDao)
+    {
+        _userDao = userDao;
+    }
+
+    public async Task<User> ValidateUser(string username, string password)
+    {
+        User? existingUser = await _userDao.GetByUsername(username);
+
+        if (existingUser == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        if (!existingUser.Password.Equals(password))
+        {
+            throw new Exception("Password mismatch");
+        }
+
+        return existingUser;
+    }
+}
