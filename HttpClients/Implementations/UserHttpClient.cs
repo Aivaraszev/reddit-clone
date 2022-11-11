2696137cc836d9b31a31144a3d2238a1c6511d6a
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-11 12:07:37 +0100

    Add a user service interface and implement it.

---
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -0,0 +1,33 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using Domain.DTOs;
+using Domain.Models;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.Implementations;
+
+public class UserHttpClient : IUserService
+{
+    private readonly HttpClient _client;
+
+    public UserHttpClient(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<User> CreateAsync(UserCreationDto dto)
+    {
+        HttpResponseMessage response = await _client.PostAsJsonAsync("/users", dto);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        User user = JsonSerializer.Deserialize<User>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return user;
+    }
+}
