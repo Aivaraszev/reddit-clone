69532165eefca46933de2ad0ff2d7d2591936411
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-22 15:01:53 +0100

    Update post to fit efc.

--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -3,7 +3,7 @@
 public class Post
 {
     public int Id { get; set; }
-    public string Title { get; init; }
-    public string Body { get; init; }
-    public User Poster { get; init; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public User Poster { get; set; }
 }
